20784abcfea69cd895b63f286c3b06f8ee35c0d4
DoukheeWon <fain9301@yahoo.com>
2025-05-16 16:06:11 +0900

    feat: first commit

---
+++ src/test/java/kr/co/won/inflearnspringintegrationstudy/InflearnSpringIntegrationStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.won.inflearnspringintegrationstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringIntegrationStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
