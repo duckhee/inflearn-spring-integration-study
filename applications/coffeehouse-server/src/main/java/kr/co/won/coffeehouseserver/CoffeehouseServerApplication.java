99f6e140eb228b17a90db16866b9e620f7b36e6f
DoukheeWon <fain9301@yahoo.com>
2025-05-19 14:42:25 +0900

    feat: project template setting multi-module

---
+++ applications/coffeehouse-server/src/main/java/kr/co/won/coffeehouseserver/CoffeehouseServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.won.coffeehouseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeehouseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeehouseServerApplication.class, args);
+    }
+
+}
---
+++ applications/coffeehouse-server/src/test/java/kr/co/won/coffeehouseserver/CoffeehouseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.won.coffeehouseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeehouseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
